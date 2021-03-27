5d86b95faaefcafc58e52ff5a7ea8ffa6bfc24b2
Jonagarme <garciajonathan268@gmail.com>
2021-03-26 23:52:56 -0500

    formulario y validaciones terminadas

---
+++ src/components/Auth/RegisterForm.js
@@ -0,0 +1,89 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { TextInput, Button } from "react-native-paper";
+import { useFormik } from "formik";
+import * as Yup from "yup";
+import { formStyle } from "../../styles";
+
+export default function RegisterForm(props) {
+  const { changeForm } = props;
+
+  const formik = useFormik({
+    initialValues: initialValues(),
+    validationSchema: Yup.object(validationSchema()),
+    onSubmit: (formData) => {
+      console.log("Registro de usuario enviado");
+      console.log(formData);
+    },
+  });
+
+  return (
+    <View>
+      <TextInput
+        label="Email"
+        style={formStyle.input}
+        onChangeText={(text) => formik.setFieldValue("email", text)}
+        value={formik.values.email}
+        error={formik.errors.email}
+      />
+      <TextInput
+        label="Nombre de Usuario"
+        style={formStyle.input}
+        onChangeText={(text) => formik.setFieldValue("username", text)}
+        value={formik.values.username}
+        error={formik.errors.username}
+      />
+      <TextInput
+        label="Contraseña"
+        style={formStyle.input}
+        secureTextEntry
+        onChangeText={(text) => formik.setFieldValue("password", text)}
+        value={formik.values.password}
+        error={formik.errors.password}
+      />
+      <TextInput
+        label="Repetir Contraseña"
+        style={formStyle.input}
+        secureTextEntry
+        onChangeText={(text) => formik.setFieldValue("repeatPassword", text)}
+        value={formik.values.repeatPassword}
+        error={formik.errors.repeatPassword}
+      />
+      <Button
+        mode="contained"
+        style={formStyle.btnSucces}
+        onPress={formik.handleSubmit}
+      >
+        Registrarse
+      </Button>
+      <Button
+        mode="text"
+        style={formStyle.btnText}
+        labelStyle={formStyle.btnTextLabel}
+        onPress={changeForm}
+      >
+        Iniciar sesíon
+      </Button>
+    </View>
+  );
+}
+
+function initialValues() {
+  return {
+    email: "",
+    username: "",
+    password: "",
+    repeatPassword: "",
+  };
+}
+
+function validationSchema() {
+  return {
+    email: Yup.string().email(true).required(true),
+    username: Yup.string().required(true),
+    password: Yup.string().required(true),
+    repeatPassword: Yup.string()
+      .required(true)
+      .oneOf([Yup.ref("password")], true),
+  };
+}
