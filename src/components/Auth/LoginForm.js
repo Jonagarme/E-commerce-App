2477640c9068d64f5e24a634f3e6e5c3e1a83ba6
Jonagarme <garciajonathan268@gmail.com>
2021-03-27 11:29:39 -0500

    login con strapin

--- src/components/Auth/LoginForm.js
+++ src/components/Auth/LoginForm.js
@@ -1,15 +1,58 @@
-import React from "react";
+import React, { useState } from "react";
 import { View, Text } from "react-native";
 import { TextInput, Button } from "react-native-paper";
+import { useFormik } from "formik";
+import * as Yup from "yup";
+import Toast from "react-native-root-toast";
+import { loginApi } from "../../api/user";
 import { formStyle } from "../../styles";
 
 export default function LoginForm(props) {
   const { changeForm } = props;
+  const [loading, setLoading] = useState(false);
+
+  const formik = useFormik({
+    initialValues: initialValues(),
+    validationSchema: Yup.object(validationSchema()),
+    onSubmit: async (formData) => {
+      setLoading(true);
+      try {
+        const response = await loginApi(formData);
+        if (response.statusCode) throw "Error en el usuario o contraseña";
+        console.log(response);
+      } catch (error) {
+        Toast.show(error, {
+          position: Toast.positions.CENTER,
+        });
+        // setLoading(false);
+      }
+      setLoading(false);
+    },
+  });
+
   return (
     <View>
-      <TextInput label="Email o Username" style={formStyle.input} />
-      <TextInput label="Contraseña" style={formStyle.input} />
-      <Button mode="contained" style={formStyle.btnSucces}>
+      <TextInput
+        label="Email o Username"
+        style={formStyle.input}
+        onChangeText={(text) => formik.setFieldValue("identifier", text)}
+        value={formik.values.identifier}
+        error={formik.errors.identifier}
+      />
+      <TextInput
+        label="Contraseña"
+        style={formStyle.input}
+        onChangeText={(text) => formik.setFieldValue("password", text)}
+        alue={formik.values.password}
+        error={formik.errors.password}
+        secureTextEntry
+      />
+      <Button
+        mode="contained"
+        style={formStyle.btnSucces}
+        onPress={formik.handleSubmit}
+        loading={loading}
+      >
         Entrar
       </Button>
       <Button
@@ -22,4 +65,18 @@ export default function LoginForm(props) {
       </Button>
     </View>
   );
+}
+
+function initialValues() {
+  return {
+    identifier: "",
+    password: "",
+  };
+}
+
+function validationSchema() {
+  return {
+    identifier: Yup.string().required(true),
+    password: Yup.string().required(true),
+  };
 }
