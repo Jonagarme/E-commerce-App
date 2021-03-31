42e51c1b1e5665cbe891f7bb89af57b0c1e7af32
Jonagarme <garciajonathan268@gmail.com>
2021-03-30 22:10:02 -0500

    formulario para cambiar name y lastname

---
+++ src/screens/Account/ChangeName.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { StyleSheet, View } from "react-native";
+import { TextInput, Button } from "react-native-paper";
+import { formStyle } from "../../styles";
+
+export default function ChangeName() {
+  return (
+    <View style={styles.container}>
+      <TextInput label="Nombre" style={formStyle.input} />
+      <TextInput label="Apellido" style={formStyle.input} />
+      <Button
+        mode="contained"
+        style={formStyle.btnSucces}
+        onPress={() => console.log("Formulario enviado")}
+      >
+        Cambiar nombre y apellidos
+      </Button>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 20,
+  },
+});
