895dedc1b34680e99b3d5905742c253f97937687
Jonagarme <garciajonathan268@gmail.com>
2021-04-02 18:46:39 -0500

    loading y mensaje que no tiene direcciones

--- src/screens/Account/Addresses.js
+++ src/screens/Account/Addresses.js
@@ -1,24 +1,27 @@
-import React, { useCallback } from "react";
+import React, { useCallback, useState } from "react";
 import {
   StyleSheet,
   View,
   ScrollView,
   Text,
   TouchableWithoutFeedback,
+  ActivityIndicator,
 } from "react-native";
 import { IconButton } from "react-native-paper";
 import { useFocusEffect } from "@react-navigation/native";
+import { set, size } from "lodash";
 import { getAddressesApi } from "../../api/address";
 import useAuth from "../../hooks/useAuth";
 
 export default function Addresses() {
+  const [addresses, setAddresses] = useState(null);
   const { auth } = useAuth();
 
   useFocusEffect(
     useCallback(() => {
       (async () => {
         const response = await getAddressesApi(auth);
-        console.log(response);
+        setAddresses(response);
       })();
     }, [])
   );
@@ -34,6 +37,13 @@ export default function Addresses() {
           <IconButton icon="arrow-right" color="#000" size={19} />
         </View>
       </TouchableWithoutFeedback>
+      {!addresses ? (
+        <ActivityIndicator size="large" style={styles.loading} />
+      ) : size(addresses) === 0 ? (
+        <Text style={styles.noAddresText}>Crea tu primera direccion</Text>
+      ) : (
+        <Text>Listado de direcciones</Text>
+      )}
     </ScrollView>
   );
 }
@@ -58,5 +68,13 @@ const styles = StyleSheet.create({
   },
   addAddresText: {
     fontSize: 16,
+  },
+  loading: {
+    marginTop: 20,
+  },
+  noAddresText: {
+    fontSize: 16,
+    marginTop: 10,
+    textAlign: "center",
   },
 });
